ceeb3e2faa6d4cf9c17f724c29d282548f8d2b67
cyd <yd-chen14@mails.tsinghua.edu.cn>
2018-03-20 13:34:24 +0800

    Finished creating examples, next to do: persuit.

--- learning.py
+++ learning.py
@@ -5,7 +5,7 @@ from paras import *
 
 def __find_by_time_indices(index, data, time, time_lag):
     by_time_indices = []
-    for i in range(index - 1, 1, -1):
+    for i in range(index - 1, 0, -1):
         time_i = data[i][0]
         if time - time_i > time_lag:
             break
@@ -17,7 +17,7 @@ def __find_by_action_indices(index, data, action, action_lag):
     by_action_indices = []
     cnt = 0
     last_action = action
-    for i in range(index - 1, 1, -1):
+    for i in range(index - 1, 0, -1):
         action_i = data[i][2:]
         if action_i == last_action:
             continue
@@ -29,7 +29,21 @@ def __find_by_action_indices(index, data, action, action_lag):
     return set(by_action_indices)
 
 
-def create_examples_with_prev_fluent(time_lag=2, action_lag=1, intersect_bool=True):
+def __ensure_consistency(by_lag_indices, data, fluent):
+    fluent_fail = 0
+    for tmp in sorted(by_lag_indices, reverse=True):
+        if data[tmp][1] != fluent:
+            fluent_fail = tmp
+            break
+    ret_by_lag_indices = []
+    if len(by_lag_indices) != 0:
+        for index in by_lag_indices:
+            if index > fluent_fail:
+                ret_by_lag_indices.append(index)
+    return ret_by_lag_indices
+
+
+def create_examples_with_prev_fluent(time_lag=3, action_lag=1, intersect_bool=True):
 
     if time_lag == 0 and action_lag == 0:
         print("ERROR: time_lag and action_lag both zero.")
@@ -37,13 +51,9 @@ def create_examples_with_prev_fluent(time_lag=2, action_lag=1, intersect_bool=Tr
 
     with open(origin_data_file) as f:
         reader = csv.reader(f)
-        z = [int(x) for x in list(reader)[1:]]
-        print(z)
-
-        exit(233)
-
-        data = np.array([1, 1, 1])
-        n_rows, n_cols = data.shape
+        data = list(reader)[1:]
+        n_rows, n_cols = np.array(data).shape
+        data = [[int(x[i]) for i in range(n_cols)] for x in data]
         inertial_index = n_cols + 1
         n_actions = n_cols - 2
 
@@ -56,7 +66,7 @@ def create_examples_with_prev_fluent(time_lag=2, action_lag=1, intersect_bool=Tr
             time = data[index][0]
             fluent = data[index][1]
             action = data[index][2:]
-            example = np.zeros(inertial_index)
+            example = [0] * inertial_index
             example[0] = time
             example[1] = fluent
             if index == 1:
@@ -73,17 +83,74 @@ def create_examples_with_prev_fluent(time_lag=2, action_lag=1, intersect_bool=Tr
                 by_lag_indices = list(by_lag_indices.difference(set(actions_in_examples)))
                 example_indices.append(index)
                 actions_in_examples.extend(by_lag_indices)
-                tmp_action = np.zeros(n_actions)
+                tmp_action = [0] * n_actions
                 if len(by_lag_indices) != 0:
                     for tmp_index in by_lag_indices:
-                        tmp_action = tmp_action | data[tmp_index][2:]
+                        for i in range(len(tmp_action)):
+                            tmp_action[i] = tmp_action[i] | data[tmp_index][2 + i]
                 example[2:-1] = tmp_action
-                new_data.append(example[1:])
+                new_data.append(example)
             last_fluent = fluent
+
+        print([data[item][0] for item in example_indices])
+        print([data[item][0] for item in actions_in_examples])
+        # 保证已经算过的有action的行不再重复算
+        for row_index in actions_in_examples:
+            for action_col in range(2, n_cols):
+                data[row_index][action_col] = 0
+
+        # 获取fluent保持一致的example
+        for index in range(n_rows-1, 0, -1):
+            # 有动作，则记录为一个example
+            if any(data[index][2:]) \
+                    and index not in example_indices:  # TODO:（之前记录过example的时间点不再记录？）
+                time = data[index][0]
+                fluent = data[index][1]
+                action = data[index][2:]
+
+                by_time_indices = __find_by_time_indices(index, data, time, time_lag)
+                by_action_indices = __find_by_action_indices(index, data, action, action_lag)
+                if time_lag > 0 and action_lag > 0 and intersect_bool:
+                    by_lag_indices = by_time_indices.intersection(by_action_indices)
+                else:
+                    by_lag_indices = by_time_indices.union(by_action_indices)
+                by_lag_indices = __ensure_consistency(by_lag_indices, data, fluent)
+                tmp_action = [0] * n_actions
+                if len(by_lag_indices) != 0:
+                    for tmp_index in by_lag_indices:
+                        for i in range(len(tmp_action)):
+                            tmp_action[i] = tmp_action[i] | data[tmp_index][2 + i]
+                inertial_example = [time, fluent]
+                inertial_example.extend(tmp_action)
+                inertial_example.append(fluent)
+                new_data.append(inertial_example)
+
+                for row_index in by_lag_indices:
+                    for action_col in range(2, n_cols):
+                        data[row_index][action_col] = 0
 
         return new_data
 
 
 new_data = create_examples_with_prev_fluent()
 
-print(new_data)
+pp.pprint(new_data)
+print(np.array(new_data).shape)
+
+
+
+# data = [[0, 0, 0, 0],
+#         [80, 0, 1, 0],
+#         [81, 0, 0, 0],
+#         [82, 1, 0, 0],  # index = 3
+#         [83, 1, 1, 0],
+#         [88, 0, 1, 0]]
+# index = 5
+# time = 88
+# time_lag = 3
+# action_lag = 1
+# action = data[index][2:]
+# # print(__find_by_time_indices(index, data, time, time_lag))
+# # print(__find_by_action_indices(index, data, action, action_lag))
+# print(data[index][2:])
+
--- paras.py
+++ paras.py
@@ -1,3 +1,4 @@
+import pprint
 
 data_path = 'data/'
 volume_file = data_path + 'volume.csv'
@@ -5,8 +6,16 @@ travel_time_file = data_path + 'travel_time.csv'
 lane_direction_file = data_path + 'lane_direction.csv'
 origin_data_file = data_path + 'origin_data.csv'    # 生成的文件
 
+pp = pprint.PrettyPrinter()
 
-# a = [[0, 1, 2], [3, 4, 5]]
-# print(a[0:][1:2])
+# a = {1, 2, 3}
+# b = list(a)
+# print(sorted(b))
 
+# a = [1, 0, 1]
+# b = [0, 0, 0]
+# print(any(b))
+# for i in range(len(a)):
+#     a[i] = a[i] | b[i]
+# print(a)
 
